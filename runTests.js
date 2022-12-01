726e0d64aa96db9999d163a279007ca4d8cec35e
CSS-Scripter <thomleenman@gmail.com>
2022-12-01 11:50:07 +0100

    recursive directories

--- runTests.js
+++ runTests.js
@@ -1,11 +1,29 @@
 import { RunAllSuites } from "./TestFramework/Suite.js";
 import { readdirSync } from "fs";
+import { join } from "path";
+
+const walkDirectory = (directory) => {
+  const specFiles = [];
+  
+  const files = readdirSync(directory, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      specFiles.push(...walkDirectory(join(directory, file.name)));
+    } else {
+      if (file.name.endsWith(".spec.js")) {
+        specFiles.push(join(directory, file.name));
+      }
+    }
+  }
+  return specFiles;
+}
 
 const runAllSpecFiles = async () => {
-  const testFiles = readdirSync("./").filter(fn => fn.endsWith(".spec.js"));
-  
+  const testFiles = walkDirectory("./");
+
   for (let file of testFiles) {
-    await import(`./${file}`);
+    const safePath = `./${file.replace("\\", "/")}`
+    await import(safePath);
   }
 
   RunAllSuites();  
